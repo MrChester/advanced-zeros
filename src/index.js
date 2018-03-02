5caf14349bd0424b3d4e6bb06de7760bff6a2af9
Eugene Romankov <iromankov2010@gmail.com>
2018-03-03 00:22:08 +0300

    Transfer isPrimeNumber function into index.js from isPrimeNumber.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ module.exports = function getZerosCount(number, base) {
         primeNumbers = [],
         maxPrimeDivisor = 0;
 
+    function isPrimeNumber(n) {
+
+        if (n === 1) {
+            return false;
+        } else if (n === 2) {
+            return true;
+        } else {
+            for (var x = 2; x < n; x++) {
+                if (n % x === 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
     for (let i = 2; i <= base; i++) {
         if (isPrimeNumber(i)) {
             primeNumbers.push(i);
