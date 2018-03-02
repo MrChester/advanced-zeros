fa0be82a0b788a38599d448b22708ab119c044c2
Eugene Romankov <iromankov2010@gmail.com>
2018-03-03 00:17:23 +0300

    Working with prime numbers and finding max prime divisor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+const isPrimeNumber = require('./isPrimeNumber');
+
 module.exports = function getZerosCount(number, base) {
-    var count = 0;
+    var count = 0,
+        primeNumbers = [],
+        maxPrimeDivisor = 0;
+
+    for (let i = 2; i <= base; i++) {
+        if (isPrimeNumber(i)) {
+            primeNumbers.push(i);
+        }
+    }
+
+    for (let i = 0; i <= base; i++) {
+        if (base % primeNumbers[i] === 0) {
+            maxPrimeDivisor = primeNumbers[i];
+        }
+    }
 
     while (number > 0) {
-        number = Math.floor(number / 5);
+        number = Math.floor(number / maxPrimeDivisor);
         count += number;
     }
 
